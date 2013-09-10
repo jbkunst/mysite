06035d5f65936e11177c3b26fefe08c53d955f71
Joshua <jbkunst@gmail.com>
2013-09-10 18:42:21 -0300

    fix desing in <a>

---
+++ sandbox/template/js/custom.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	$(".git-hub").click(function() {
+		window.open('https://github.com/jbkunst', '_blank');
+	});
+
+	$(".last-fm").click(function() {
+		window.open('http://www.lastfm.com/user/jbkunst', '_blank');
+	});
+
+	$(".stackoverflow").click(function() {
+		window.open('http://stackoverflow.com/users/829971/jbkunst', '_blank');
+	});
+
+	$(".twitter").click(function() {
+		window.open('https://twitter.com/jbkunst', '_blank');
+	});
+
+	$(".google-plus").click(function() {
+		window.open('', '_blank');
+	});
+
+	$(".linkedin").click(function() {
+		window.open('http://cl.linkedin.com/in/joshuakunst', '_blank');
+	});
+});
