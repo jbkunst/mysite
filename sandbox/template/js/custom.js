f476d9306570206eed7ec7fc7cfb61fee469616c
Joshua <jbkunst@gmail.com>
2014-01-09 15:45:47 -0300

    some changes in post template

--- sandbox/template/js/custom.js
+++ sandbox/template/js/custom.js
@@ -1,25 +1,3 @@
-$(document).ready(function() {
-	$(".git-hub").click(function() {
-		window.open('https://github.com/jbkunst', '_blank');
-	});
+$(function(){
 
-	$(".last-fm").click(function() {
-		window.open('http://www.lastfm.com/user/jbkunst', '_blank');
-	});
-
-	$(".stackoverflow").click(function() {
-		window.open('http://stackoverflow.com/users/829971/jbkunst', '_blank');
-	});
-
-	$(".twitter").click(function() {
-		window.open('https://twitter.com/jbkunst', '_blank');
-	});
-
-	$(".google-plus").click(function() {
-		window.open('', '_blank');
-	});
-
-	$(".linkedin").click(function() {
-		window.open('http://cl.linkedin.com/in/joshuakunst', '_blank');
-	});
 });
