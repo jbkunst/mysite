aa68d97ec09a8e77a152d5a8f4cfa3f2f944fc1e
Joshua <jbkunst@gmail.com>
2014-05-22 18:13:52 -0400

    themes

--- mysite/context_processors.py
+++ mysite/context_processors.py
@@ -9,7 +9,7 @@ def themes(request):
 	themes = ['forest', 'clouds']
 	selection = datetime.datetime.now().hour % len(themes)
 	theme = themes[selection]
-	theme = themes[random.choice([0,1])]
+	# theme = themes[random.choice([0,1])]
  	theme_css = settings.STATIC_URL + "css/" + "custom_theme_%s.css" % theme
 
  	return { "theme_css" : theme_css }
