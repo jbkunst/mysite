397301ed61d0a418577c7f4dae3aaf52e541b6b0
Joshua <jbkunst@gmail.com>
2014-05-26 00:02:56 -0400

    add yellow theme

--- mysite/context_processors.py
+++ mysite/context_processors.py
@@ -6,10 +6,10 @@ import random
 
 def themes(request):
 
-	themes = ['forest', 'clouds']
+	themes = ['forest', 'clouds', 'forest_yellow']
 	selection = datetime.datetime.now().hour % len(themes)
 	theme = themes[selection]
-	# theme = themes[random.choice([0,1])]
+	# theme = themes[random.choice(xrange(len(themes)))]
  	theme_css = settings.STATIC_URL + "css/" + "custom_theme_%s.css" % theme
 
  	return { "theme_css" : theme_css }
