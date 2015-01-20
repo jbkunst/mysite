282607ffbd2a590d4dff90caac6e99ffb16ecbb0
Joshua <jbkunst@gmail.com>
2015-01-20 11:45:56 -0300

    testing new template project

--- sandbox/old_site/blog/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from blog.models import Contact
-
-class ContactForm(ModelForm):
-	class Meta:
-		model = Contact
-		fields = ('name', 'email', 'message')
-		#widgets = {'name': Textarea(attrs={'cols': 80, 'rows': 20}),}
